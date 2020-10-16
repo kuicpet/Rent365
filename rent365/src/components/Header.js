f08e24101348bb252d4eb861520c8f99b5f07134
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-16 20:27:24 +0100

    ft--updated

--- rent365/src/components/Header.js
+++ rent365/src/components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
                           <Nav.Link>
                               <Link to="/" className="mx-5 home">Home</Link>
                           </Nav.Link>
-                          <Navbar.Text>Signed in as: <a href="#">John Doe</a></Navbar.Text>
+                          <Navbar.Text>Signed in as: <span>John Doe</span></Navbar.Text>
                           <Nav.Link>
                               <Link
                                 to="/"
@@ -45,17 +45,23 @@ const Header = () => {
                           </Nav.Link>
                           <NavDropdown title="Login" class="drop">
                             <NavDropdown.Item>
-                                <Link to="/signin" className="text-center signin">Login</Link>
+                                <Link to="/signin" className="text-center signin"><button className="px-5 py-1 signinBtn">Login</button></Link>
                             </NavDropdown.Item>
                             <NavDropdown.Item>
-                                <Link to="/signup" className="text-center signup">Create an Account</Link>
+                                <Link to="/signup" className="text-center signup"><button className="px-3 py-1 signupBtn">Create an Account</button></Link>
                             </NavDropdown.Item>
                             <NavDropdown.Divider/>
                             <NavDropdown.Item>
-                                <Link to="/orders" className="text-center orders">Orders</Link>
+                                <Link to="/orders" className="text-center orders">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg>
+                                    <span className="mx-2">Orders</span>
+                                </Link>
                             </NavDropdown.Item>
                             <NavDropdown.Item>
-                                <Link to="/saved-items" className="text-center saved-items">Saved Items</Link>
+                                <Link to="/saved-items" className="text-center saved-items">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                    <span className="mx-2">Saved Items</span>
+                                </Link>
                             </NavDropdown.Item>
                           </NavDropdown>
                           <Nav.Link>
