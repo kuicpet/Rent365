15bc58d98688ff49d7ab0af453317b948544b8f1
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-16 12:34:39 +0100

    bg--updated header

--- rent365/src/components/Header.js
+++ rent365/src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
     }
 
     return (
-         <Navbar bg="light" expand="lg">
+         <Navbar bg="light" expand="lg" fixed="top">
              <Navbar.Brand>
                  <Link to="/" className="navbar-brand">Rent365</Link>
              </Navbar.Brand>
@@ -27,12 +27,13 @@ const Header = () => {
                      {isLoggedIn() ? (
                         <Nav>
                           <Nav.Link>
-                              <Link to="/" className="mx-5">Home</Link>
+                              <Link to="/" className="mx-5 home">Home</Link>
                           </Nav.Link>
+                          <Navbar.Text>Signed in as: <a href="#">John Doe</a></Navbar.Text>
                           <Nav.Link>
                               <Link
                                 to="/"
-                                className="mx-2"
+                                className="mx-2 signout"
                                 onClick={() => signOut()}
                               >Sign Out</Link>
                           </Nav.Link>
@@ -40,25 +41,28 @@ const Header = () => {
                      ) : (
                        <Nav>
                           <Nav.Link>
-                              <Link to="/" className="mx-5">Home</Link>
+                              <Link to="/" className="mx-5 home">Home</Link>
                           </Nav.Link>
-                          <NavDropdown title="Login">
+                          <NavDropdown title="Login" class="drop">
                             <NavDropdown.Item>
-                                <Link to="/signin" className="text-center">Login</Link>
+                                <Link to="/signin" className="text-center signin">Login</Link>
                             </NavDropdown.Item>
                             <NavDropdown.Item>
-                                <Link to="/signin" className="text-center">Create an Account</Link>
+                                <Link to="/signup" className="text-center signup">Create an Account</Link>
                             </NavDropdown.Item>
                             <NavDropdown.Divider/>
                             <NavDropdown.Item>
-                                <Link to="/" className="text-center">Orders</Link>
+                                <Link to="/orders" className="text-center orders">Orders</Link>
                             </NavDropdown.Item>
                             <NavDropdown.Item>
-                                <Link to="/signin" className="text-center">Saved Items</Link>
+                                <Link to="/saved-items" className="text-center saved-items">Saved Items</Link>
                             </NavDropdown.Item>
                           </NavDropdown>
                           <Nav.Link>
-                              <Link to="/cart" className="mx-5">Cart</Link>
+                              <Link to="/cart" className="mx-5 cart">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                                  <span className="mx-1">Cart</span>
+                              </Link>
                           </Nav.Link>
                        </Nav>
                      )}
