f4941093184b7ce81751c1895ae052a858336cec
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-17 07:54:09 +0100

    ft--added search

--- rent365/src/components/Search.js
+++ rent365/src/components/Search.js
@@ -4,16 +4,16 @@ import React from "react";
 const Search = () => {
     return (
         <div className="row search">
-            <div className="col-sm-12">
+            <div className="col-sm-12 col-md-12 col-lg-12">
                 <form className="form">
-                    <input 
+                    <input
                         type="text"
                         name="product"
                         id="search"
                         placeholder="E.g Search products,services"
                         required
                     />
-                    <button className="mx-1 px-4 py-1 searchBtn" type="submit">Search</button>
+                    <button className="m-1 px-4 py-1 searchBtn" type="submit">Search</button>
                 </form>
             </div>
         </div>
