5a88b36be0fb5c3d8e10bd9668a3787cd8fc198e
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-18 21:58:40 +0100

    ft--added counter

--- rent365/src/components/Counter.js
+++ rent365/src/components/Counter.js
@@ -23,7 +23,7 @@ const Counter = () => {
                 <button onClick={handleDecrement}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                 </button>
-                <span className="countNum">{count}</span>
+                <span className="mx-3 countNum">{count}</span>
                 <button onClick={handleIncrement}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                 </button>
