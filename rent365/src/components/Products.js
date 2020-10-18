23e26be73574b11ecf047398c6a00cc53054b8ea
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-18 14:09:12 +0100

    ft--products added and fetched from api

--- rent365/src/components/Products.js
+++ rent365/src/components/Products.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 
 const Products = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://cors-anywhere.herokuapp.com/http://api-rent365.herokuapp.com/shop/api/items-list")
+            .then(result => setData(result.data));
+    },[]);
+    
     return (
       <div className="row products">
-          <div className="col-sm-12">
-              <h2>Products</h2>
-          </div>
+          {data.map((item, index) => (
+              <div key={index} className="col-sm-12 col-md-3 col-lg-4">
+                  <Link>
+                    
+                  </Link>
+                  <img
+                    src={item.item_pic}
+                    alt={item.title}
+                  />
+                  <h4>{item.title}</h4>
+                  <p>$ {item.price}</p>
+                  <p>Description: {item.description}</p>
+              </div>
+          ))}
       </div>
     );
 }
