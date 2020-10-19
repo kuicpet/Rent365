7e157f64c08a087de0b1d3dbddec8a623340f466
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-19 19:46:29 +0100

    bg--updated

--- rent365/src/components/Footer.js
+++ rent365/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-      <div className="row footer my-2">
+      <div className="row footer my-2 text-center">
           <div className="col-sm-12">
-              <small>Rent365 &copy;2020 <span className="mx-2">Terms and Conditions</span></small>
+              <small>Rent365 &copy;2020</small>
           </div>
       </div>
     );
