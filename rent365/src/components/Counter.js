ca4d52d36fa76fba6cc65dd81bf2a556021a5c5a
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-19 04:54:24 +0100

    ft--updated counter

--- rent365/src/components/Counter.js
+++ rent365/src/components/Counter.js
@@ -20,13 +20,16 @@ const Counter = () => {
     return (
         <div className="row text-center">
             <div className="col-sm-12 py-2 counter">
-                <button onClick={handleDecrement}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                <span>
+                <button onClick={handleDecrement} className="minusBtn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                 </button>
-                <span className="mx-3 countNum">{count}</span>
-                <button onClick={handleIncrement}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                <input className="countNum"
+                value={count}/>
+                <button onClick={handleIncrement} className="addBtn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                 </button>
+                </span>
             </div>
         </div>
     );
