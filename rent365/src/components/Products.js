41d40e22934093dd55d936d044137716af7cce71
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-19 05:17:07 +0100

    ft--updated counter

--- rent365/src/components/Products.js
+++ rent365/src/components/Products.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import ReactLoading from "react-loading";
 import Counter from "./Counter";
 
 
@@ -8,17 +9,21 @@ import Counter from "./Counter";
 
 const Products = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+      setLoading(true);
       axios
         .get("https://cors-anywhere.herokuapp.com/http://api-rent365.herokuapp.com/shop/api/items-list")
         .then(result => setData(result.data));
+      setLoading(false);
     },[]);
     
     return (
       <div className="products mb-5">
           {data.map((item, index) => (
             <div key={index} className="col-sm-12 col-md-4 col-lg-4 product">
+              <ReactLoading/>
               <Link>
                   <img
                     src={item.item_pic}
