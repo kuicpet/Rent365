a7e99f8c42219c67bfc526e93a2619b087397b6c
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-20 20:03:07 +0100

    bg--updated

--- rent365/src/components/Cart.js
+++ rent365/src/components/Cart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Heading from "./Heading";
+import Header from "./Header";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 const Cart = () => {
     return (
         <div className="row text-center">
-            <Heading/>
+            <Header/>
             <div className="col-sm-12 cartSection">
                 <h3 className="mt-2">Cart</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-shopping-cart shopCart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
