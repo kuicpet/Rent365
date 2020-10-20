775480ffa8d72ea294ebc05fbfe42c78746ba049
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-20 09:28:48 +0100

    bg--updated header for user

--- rent365/src/components/Header.js
+++ rent365/src/components/Header.js
@@ -39,6 +39,26 @@ const Header = () => {
                               <Link to="/" className="mx-5 home">Home</Link>
                           </Nav.Link>
                           <Navbar.Text>Signed in as: <span>{user.username}</span></Navbar.Text>
+                          <NavDropdown title="My Account">
+                              <NavDropdown.Item>
+                                <Link to="/account" className="text-center account">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                                    <span className="mx-2">Account</span>
+                                </Link>
+                              </NavDropdown.Item>
+                              <NavDropdown.Item>
+                                <Link to="/orders" className="text-center orders">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg>
+                                    <span className="mx-2">Orders</span>
+                                </Link>
+                              </NavDropdown.Item>
+                              <NavDropdown.Item>
+                                  <Link to="/saved-items" className="text-center saved-items">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                      <span className="mx-2">Saved Items</span>
+                                  </Link>
+                              </NavDropdown.Item>
+                          </NavDropdown>
                           <Nav.Link>
                               <Link
                                 to="/"
