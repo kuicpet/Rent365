dad6538dfa7a2f41fabd88d16ac51cd527dd1a2e
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-21 06:13:09 +0100

    bg--updated

--- rent365/src/components/Account.js
+++ rent365/src/components/Account.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Account = () => {
     return (
         <div className="row text-center">
             <Header />
-            <div className="col-sm-12 accountSection">
+            <div className="col-sm-12 col-md-3 col-lg-3 side-nav">
+                <h3>Side Nav</h3>
+                <Nav className="nav-items">
+                   <Link to="/customer/account">My Rent365 Account</Link>
+                   <Link to="/customer/order">Orders</Link>
+                   <Link to="/customer/saved-items">Saved Items</Link>
+                   <Link>Sign Out</Link>
+                </Nav>
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-8 accountSection">
                 <h3>Account</h3>
             </div>
             <Footer />
